e9b032b19b74ee2e2962dcf4e6904acdf08f4bdb
Jeremiah Barth <jbarth1986@gmail.com>
2023-04-17 22:31:52 -0500

    Day 26 updates

---
+++ April/4-17-2023-day-26/README.md
@@ -0,0 +1,3 @@
+# Day 26 - April 17, 2023
+
+Still plodding along slowly but surely. I'm realizing that with my current commitments my progress back to where I once was isn't going to be as quick as I'd like. But that's ok I guess. As I said before, slow progress is better than no progress.
--- React-sandbox/fcc-course/src/Components/Header.jsx
+++ React-sandbox/fcc-course/src/Components/Header.jsx
@@ -1,15 +1,12 @@
-import hovalin from "../Images/hovalin.png";
+import logo from "../logo.svg";
 
 export default function Header() {
     return (
         <header>
             <nav className="nav">
-                <img src={hovalin} />
-                <ul className="nav-items">
-                    <li>Pricing</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
+                <img className="logo-img" src={logo} />
+                <h3 className="logo-text">ReactFacts</h3>
+                <h4 className="page-name">React Course - Project 1</h4>
             </nav>
         </header>
     );
